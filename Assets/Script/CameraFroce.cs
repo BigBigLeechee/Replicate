504db5a586078c8d85b38e5911aae17cd537e31e
EvilRose <591243801@qq.com>
2017-11-22 16:53:41 +0800

    iiii

---
+++ Assets/Script/CameraFroce.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFroce : MonoBehaviour {
+
+    public float minDistance = 5f;  // 玩家和镜头的最小距离
+    public Transform playerTransform;//玩家的位置
+    public float speed = 3f; //镜头移动的速度
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Vector2.Distance(this.transform.position,playerTransform.position) < minDistance){
+            MoveToPosition();
+        }
+	}
+
+	/**
+	使得镜头移动到相应的地方
+	 */
+	private void MoveToPosition(){
+        //获取将要去到的地点
+        Vector3 target = this.transform.position;
+        target.x += minDistance;
+
+		//移动到将要去到的地点
+        this.transform.position =
+        	 Vector3.MoveTowards(this.transform.position, target, speed * Time.deltaTime);
+    }
+}
--- Assets/Script/Player/PlayController.cs
+++ Assets/Script/Player/PlayController.cs
@@ -7,16 +7,20 @@ public class PlayController : MonoBehaviour {
 	private static float JUMP_LIMIT = 0.4f;  //跳跃的音量限制
 	private static float MOVE_LIMIT = 0.1f; //移动的音量限制
 
-    public float moveFroce = 5f;
-    public float jumpFroce = 50f;
+    public float jumpCDTimer = 2f;      //跳跃的CD
+    private float jumpTimer = 0f;
+
+    public float moveFroce = 5f;        //移动的力
+    public float jumpFroce = 50f;       //跳跃的力
 
     public float maxMoveSpeed = 5f;    //最大的移动速度
-	public Animator playerAnimator;   //控制玩家动画的控制器
+	private Animator mPlayerAnimator;   //控制玩家动画的控制器
 
     private Rigidbody2D mRigidbody;
     // Use this for initialization
     void Start () {
 		mRigidbody = GetComponent<Rigidbody2D>();
+        mPlayerAnimator = GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
@@ -24,11 +28,13 @@ public class PlayController : MonoBehaviour {
 		//获取音量
 		float volume = MicrophoneInput.Instance.getVolume();
 
-		if(volume > JUMP_LIMIT){
+        jumpTimer += Time.deltaTime;
+
+        if(volume > JUMP_LIMIT && jumpTimer > jumpCDTimer){
 			//TODO: Jump
 			PlayerJump();
-		}
-
+            jumpTimer = 0f;
+        }
 		if(volume > MOVE_LIMIT){
 			//TODO: Move
 			PlayerMove();
@@ -42,22 +48,25 @@ public class PlayController : MonoBehaviour {
 	 */
 	private void PlayerMove(){
 
-        mRigidbody.AddForce(Vector2.right * moveFroce, ForceMode2D.Impulse);
+        mRigidbody.AddForce(Vector2.right * moveFroce, ForceMode2D.Force);
 
         if(mRigidbody.velocity.x > maxMoveSpeed){
 			mRigidbody.velocity = new Vector2(maxMoveSpeed , mRigidbody.velocity.y);
 		}
 
-        playerAnimator.SetInteger("Move", 1);
+        mPlayerAnimator.SetTrigger("Move");
     }
 
 	/**
 	玩家跳跃的方法
 	 */
 	private void PlayerJump(){
-        mRigidbody.AddForce(Vector2.up * jumpFroce, ForceMode2D.Impulse);
+        mRigidbody.AddForce(Vector2.up * jumpFroce, ForceMode2D.Force);
 
-        playerAnimator.SetInteger("Jump", 0);
+        mPlayerAnimator.SetTrigger("Jump");
     }
+
+
+
 
 }
