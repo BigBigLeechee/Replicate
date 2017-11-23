aa57fbed8a41d025db4fdc2cc9976729a6f11159
EvilRose <591243801@qq.com>
2017-11-23 17:59:06 +0800

    加入了开始界面

---
+++ Assets/Script/UI/HelpUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelpUI : MonoBehaviour {
+
+    private Button returnButton;
+    // Use this for initialization
+    void Start () {
+        returnButton = GetComponentInChildren<Button>();
+
+        returnButton.onClick.AddListener(() =>
+        {
+            this.gameObject.SetActive(false);
+        });
+    }
+}
